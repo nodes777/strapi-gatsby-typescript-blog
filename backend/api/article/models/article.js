80bffad71ad87a92eeb076e4fc31e3d75f5655f8
Taylor <taylor.nodell@gmail.com>
2020-10-28 15:00:49 +1030

    adds photo content type adds env to gignore

--- backend/api/article/models/article.js
+++ backend/api/article/models/article.js
@@ -5,66 +5,19 @@ const fetch = require("cross-fetch");
  */
 
 module.exports = {
-  // Before saving a value.
-  // Fired before an `insert` or `update` query.
-  beforeSave: async (input) => {
-    const res = await fetch(
-      `https://api.inaturalist.org/v1/observations/${input.iNatID}`
-    );
-    if (res.status >= 400) {
-      throw new Error("Bad response from iNaturalist server");
-    }
-    const d = await res.json();
-    const iNatData = d.results[0];
-    console.log(iNatData);
-
-    console.log(input.placeGuess);
-    input.placeGuess = iNatData.place_guess;
-    console.log(input.placeGuess);
-  },
-  // beforeSave: async (model, attrs, options) => {},
-
-  // After saving a value.
-  // Fired after an `insert` or `update` query.
-  // afterSave: async (model, response, options) => {},
-
-  // Before fetching a value.
-  // Fired before a `fetch` operation.
-  // beforeFetch: async (model, columns, options) => {},
-
-  // After fetching a value.
-  // Fired after a `fetch` operation.
-  // afterFetch: async (model, response, options) => {},
-
-  // Before fetching all values.
-  // Fired before a `fetchAll` operation.
-  // beforeFetchAll: async (model, columns, options) => {},
-
-  // After fetching all values.
-  // Fired after a `fetchAll` operation.
-  // afterFetchAll: async (model, response, options) => {},
-
-  // Before creating a value.
-  // Fired before an `insert` query.
-  // beforeCreate: async (model, attrs, options) => {},
-
-  // After creating a value.
-  // Fired after an `insert` query.
-  // afterCreate: async (model, attrs, options) => {},
-
-  // Before updating a value.
-  // Fired before an `update` query.
-  // beforeUpdate: async (model, attrs, options) => {},
-
-  // After updating a value.
-  // Fired after an `update` query.
-  // afterUpdate: async (model, attrs, options) => {},
-
-  // Before destroying a value.
-  // Fired before a `delete` query.
-  // beforeDestroy: async (model, attrs, options) => {},
-
-  // After destroying a value.
-  // Fired after a `delete` query.
-  // afterDestroy: async (model, attrs, options) => {}
+  // lifecycles: {
+  //   async beforeCreate(input) {
+  //     const res = await fetch(
+  //       `https://api.inaturalist.org/v1/observations/${input.iNatID}`
+  //     );
+  //     if (res.status >= 400) {
+  //       throw new Error("Bad response from iNaturalist server");
+  //     }
+  //     const d = await res.json();
+  //     const iNatData = d.results[0];
+  //     console.log(input.placeGuess);
+  //     input.placeGuess = iNatData.place_guess;
+  //     console.log(input.placeGuess);
+  //   },
+  // },
 };
---
+++ backend/api/photo/models/photo.js
@@ -0,0 +1,26 @@
+"use strict";
+const fetch = require("cross-fetch");
+/**
+ * Lifecycle callbacks for the `photo` model.
+ */
+
+module.exports = {
+  lifecycles: {
+    async beforeCreate(input) {
+      const res = await fetch(
+        `https://api.inaturalist.org/v1/observations/${input.iNatID}`
+      );
+      if (res.status >= 400) {
+        throw new Error("Bad response from iNaturalist server");
+      }
+      const d = await res.json();
+      const iNatData = d.results[0];
+
+      console.log(iNatData);
+
+      console.log(input.placeGuess);
+      input.placeGuess = iNatData.place_guess;
+      console.log(input.placeGuess);
+    },
+  },
+};
