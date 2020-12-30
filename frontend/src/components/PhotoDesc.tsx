cb3a383a5ae45018e9b2e7e7bce2e10747a29d9f
Taylor <taylor.nodell@gmail.com>
2020-12-31 10:07:04 +1030

    moves photodesc css to own file

--- frontend/src/components/PhotoDesc.tsx
+++ frontend/src/components/PhotoDesc.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../styles/photoPage.module.css'
+import styles from '../styles/photoDesc.module.css'
 import { PhotoInterface } from '../typings'
 import ReactMarkdown from 'react-markdown'
 interface PhotoDescProps {
@@ -9,24 +9,26 @@ const PhotoDesc: React.FC<PhotoDescProps> = ({ photoData }) => {
   return (
     <>
       <h1 className={styles.title}>{photoData.title}</h1>
-      <div className={styles.iNatData}>
-        <div>
-          <p className={styles.commonName}>{photoData.iNatData.commonName}</p>
-          <p className={styles.latinName}>{photoData.iNatData.latinName}</p>
+      <div className={styles.colDataAndText}>
+        <div className={styles.iNatData}>
+          <div>
+            <p className={styles.commonName}>{photoData.iNatData.commonName}</p>
+            <p className={styles.latinName}>{photoData.iNatData.latinName}</p>
+          </div>
+          <div>
+            <p>{photoData.iNatData.placeGuess}</p>
+            <p>
+              {new Date(photoData.iNatData.dateTaken).toLocaleDateString('en-gb', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+              })}
+            </p>
+          </div>
         </div>
-        <div>
-          <p>{photoData.iNatData.placeGuess}</p>
-          <p>
-            {new Date(photoData.iNatData.dateTaken).toLocaleDateString('en-gb', {
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric'
-            })}
-          </p>
+        <div className={styles.descriptionContent}>
+          <ReactMarkdown source={photoData.content} />
         </div>
-      </div>
-      <div className={styles.descriptionContent}>
-        <ReactMarkdown source={photoData.content} />
       </div>
     </>
   )
