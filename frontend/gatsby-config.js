e63b7741131b457d626ef27242d5841b4eb419e6
nodes777 <nodes777@gmail.com>
2022-05-06 21:04:08 +1000

    fixes forceBase64Format Err and ESM Import Error in Remark-mdx

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
       options: {
         // Available options and their defaults:
         base64Width: 20,
-        forceBase64Format: ``, // valid formats: png,jpg,webp
+        forceBase64Format: 'jpg', // valid formats: png,jpg,webp
         useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
         stripMetadata: true,
         // upped from 50
