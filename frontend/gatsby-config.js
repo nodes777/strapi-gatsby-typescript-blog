2632c2ec8c17751d2f05fd26aca8f894fcdfd4b3
nodes777 <nodes777@gmail.com>
2022-05-13 14:08:56 +1000

    fixes!

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -18,11 +18,12 @@ module.exports = {
       resolve: 'gatsby-source-strapi',
       options: {
         apiURL: 'http://localhost:1337',
-        contentTypes: [
-          // List of the Content Types you want to be able to request from Gatsby.
-          'photo',
-          'tag'
-        ],
+        collectionTypes: ['photo', 'tag'],
+        // contentTypes: [
+        //   // List of the Content Types you want to be able to request from Gatsby.
+        //   'photo',
+        //   'tag'
+        // ],
         queryLimit: 1000
       }
     },
